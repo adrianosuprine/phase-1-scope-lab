230fb21ee532b9b7a4a4e83d4eaae5ae0d68d356
adrianosuprine <adrianosuprine@gmail.com>
2023-12-20 13:01:27 +0300

    Test the scope

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = "bob";
+// const leastFavoriteCustomer = "mercy";
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+var bestCustomer;
+function setBestCustomer(){
+  return bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+ return bestCustomer = "maybe bob";
+}
+const leastFavoriteCustomer = "mercy";
+function changeLeastFavoriteCustomer(){
+  leastFavoriteCustomer = "ruth";
+}
+
